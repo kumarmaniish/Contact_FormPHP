146f5c78b9e512323069c5176c33e801fd0c4d91
Manish Kumar <133526263+kumarmaniish@users.noreply.github.com>
2024-04-30 11:51:03 +0530

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,31 @@
+<php
+$dayNumber = 3;
+
+switch ($dayNumber) {
+    case 1:
+        $dayName = "Monday";
+        break;
+    case 2:
+        $dayName = "Tuesday";
+        break;
+    case 3:
+        $dayName = "Wednesday";
+        break;
+    case 4:
+        $dayName = "Thursday";
+        break;
+    case 5:
+        $dayName = "Friday";
+        break;
+    case 6:
+        $dayName = "Saturday";
+        break;
+    case 7:
+        $dayName = "Sunday";
+        break;
+    default:
+        $dayName = "Invalid day";
+}
+
+echo "The day is $dayName.";
+?>
